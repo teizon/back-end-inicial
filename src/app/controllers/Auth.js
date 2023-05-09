1186f76c9f9a6350cf96b43155c36bc6a9a8949f
Tiago Lage <tiagolage7@gmail.com>
2023-05-09 15:27:28 -0300

    criando a rota de login de usuário

--- src/app/controllers/Auth.js
+++ src/app/controllers/Auth.js
@@ -1,11 +1,19 @@
 import { Router } from 'express';
 import bcrypt from "bcryptjs";
 import crypto from "crypto";
+import authConfig from "@/config/auth";
 import jwt from "jsonwebtoken";
 import user from "@/app/schemas/user";
 import { error } from 'console';
 
 const router = new Router();
+
+const generateToken = params => {
+    return jwt.sign(params, authConfig.secret, {
+            expiresIn: 86400 //expira em um dia
+        }
+    );     
+}
 
 router.post("/register", (req, res) => {
     const { email, name, password } = req.body;
@@ -31,6 +39,32 @@ router.post("/register", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
+    const {email, password } = req.body;
+
+    user.findOne({ email })
+    .select("+password")
+        .then( user => {
+            if (user){ 
+                bcrypt.compare(password, user.password)
+                .then(result =>{
+                    if (result){
+                        const token = generateToken({uid: user.id})
+                        return res.send({token: token, tokenExpiration: "1d"}); //1w, 1y, infinity                           
+                    }else{
+                        return res.status(400).send({error: "invalid password"});
+                    }
+                }).catch(error => {
+                    console.error("erro ao vereficaar a senha", error);
+                    return res.status(500).send({error: "internal server error"})
+                })
+            }else{ 
+                return res.status(404).send({error: "user not found"});
+            }
+        })
+        .catch( error => {
+            console.log("erro ao loggar", error);
+            return res.status(500).send({error: "internal server error"});
+        })
 
 });
 
