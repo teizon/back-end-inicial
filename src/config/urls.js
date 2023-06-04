74b393ef086463ccf18379b4ba2f73d3c713dc50
Tiago Lage <tiagolage7@gmail.com>
2023-06-04 17:40:29 -0300

    documentação pronta, excecao do post de + 1 imagem

--- src/config/urls.js
+++ src/config/urls.js
@@ -2,27 +2,32 @@
 
 var urls;
 
-switch (process.env.NODE_ENV) {
-  case 'production': //No servidor de produção
+/*switch (process.env.NODE_ENV) {
+  /*case 'production': //No servidor de produção
     urls = {
-      frontUrl: 'https://www.compjunior.com.br',
+      //frontUrl: 'https://www.compjunior.com.br',
       backUrl: 'https://www.compjunior.com.br/api',
     };
     break;
   case 'development': //No servidor de testes
     urls = {
-      frontUrl: 'https://squad.compjunior.com.br',
+      //frontUrl: 'https://squad.compjunior.com.br',
       backUrl: 'https://squad.compjunior.com.br/api',
     };
     break;
   default:
     //No computador local
     urls = {
-      frontUrl: 'http://localhost:8080',
+      //frontUrl: 'http://localhost:8080',
       backUrl: 'http://localhost:3000',
     };
     break;
-}
+}*/
+
+urls = {
+  //frontUrl: 'http://localhost:8080',
+  backUrl: 'http://localhost:3000',
+};
 
 //Note que a url para /uploads é diferente nos servidores, pois o servidor tem um redirecionamento.
 //Onde deveria ser /api/uploads, é redirecionado para apenas /uploads
